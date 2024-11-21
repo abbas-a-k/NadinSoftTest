9f4cfa645d3abd7c93ff062eaaf6889cd63c6f26
abbas-a-k <a2cuber20205020@gmail.com>
2024-11-21 22:02:19 +0330

    Create userController

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using api.Model;
+using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
